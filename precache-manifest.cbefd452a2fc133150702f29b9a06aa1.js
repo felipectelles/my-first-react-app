963a0960fcd9d9670ee6f137e583adcd0dc0674d
Felipe Telles <felipecorreatelles@gmail.com>
2019-06-30 14:26:17 -0300

    Updates

---
+++ precache-manifest.cbefd452a2fc133150702f29b9a06aa1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab77870a6d9bbeeefb75b47692d626e",
+    "url": "/felipectelles/my-first-react-app.git/index.html"
+  },
+  {
+    "revision": "03fafb7a96d9e063aa8a",
+    "url": "/felipectelles/my-first-react-app.git/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/felipectelles/my-first-react-app.git/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "03fafb7a96d9e063aa8a",
+    "url": "/felipectelles/my-first-react-app.git/static/js/main.02beb2ff.chunk.js"
+  },
+  {
+    "revision": "b9d2e307d42f53aa3c28",
+    "url": "/felipectelles/my-first-react-app.git/static/js/runtime~main.33374704.js"
+  }
+]);
